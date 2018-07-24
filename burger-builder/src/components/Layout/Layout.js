335cc3234a2859cdce2e30864ec5ad457db649f3
korrawitj <korrawit.jaijit@gmail.com>
2018-07-24 14:44:36 +0700

    calculate dynamic

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import Aux from '../../hoc/Aux';
+import Children from '../../hoc/Children';
 import classes from './Layout.css';
 
 const layout = (props) => (
-    <Aux>
+    <Children>
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
-    </Aux>
+    </Children>
 ); 
 
 export default layout;
