c836306e33e0b971a3df25631d6dc0b4047a737f
korrawitj <korrawit.jaijit@gmail.com>
2018-07-25 17:47:37 +0700

    Add Logo and Toolbar

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import Children from '../../hoc/Children';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Children>
+        <Toolbar />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
