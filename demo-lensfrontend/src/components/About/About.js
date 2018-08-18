5eaab83b988798d691c0d215afd616a779306a67
Korrawit Jaijit <korrawit.jaijit@gmail.com>
2018-08-18 14:06:19 +0700

    Update about us

--- demo-lensfrontend/src/components/About/About.js
+++ demo-lensfrontend/src/components/About/About.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import swiper from '../../assets/images/swiper/2.jpg';
+import './About.css'
+
 const about = (props) => (
-    <div style={{marginTop: '80px'}}>
-        <p><strong>ทดสอบหน้า</strong></p>
+    <div style={{ marginTop: '80px' }}>
+        <div className="content-wrap">
+            <div id="myCarousel" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                </ol>
+            </div>
+            <div className="carousel-inner">
+                <div className="item active">
+                    <img src={swiper} alt="LensLineUp" style={{ width: '100%' }} />
+                    <div className="carousel-caption">
+                        <h3>LensLineUp</h3>
+                        <p>ติดต่อเรา</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 );
 
