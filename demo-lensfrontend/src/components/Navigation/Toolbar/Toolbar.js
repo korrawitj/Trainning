1d1f525b701c321054ae26368ea12b612e340156
Gunnpj <gunpengpinij@gmail.com>
2018-08-18 14:08:45 +0700

    sadad

--- demo-lensfrontend/src/components/Navigation/Toolbar/Toolbar.js
+++ demo-lensfrontend/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,32 +2,28 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import HomeLogo from '../../../assets/images/LogoHere.jpg';
-
+import './Toolbar.css';
 const toolbar = (props) => (
     <header>
         <nav className="navbar navbar-expand-sm bg-light navbar-light fixed-top">
             <div className="container">
-                <NavLink className="navbar-brand" exact to="/">
-                    <img src={HomeLogo} alt="Logo" style={{ width: '80px' }} />
-                </NavLink>
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <NavLink exact to="/" className="nav-link">หน้าหลัก</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled" href="">บริการหลังการขาย</a>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/About" className="nav-link">เกี่ยวกับเรา</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/Login" className="btn btn-outline-primary">เข้าสู่ระบบ</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <i className="fa fa-cart-arrow-down nav-link" style={{fontSize: '25px', cursor: 'pointer'}}></i>
-                    </li>
 
-                </ul>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <NavLink className="navbar-brand" exact to="/">
+                        <img src={HomeLogo} alt="Logo" className="logo" />
+                    </NavLink>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-item nav-link active">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-item nav-link">Features</a>
+                            <a className="nav-item nav-link"> Pricing</a>
+                            <a className="nav-item nav-link disabled" >Disabled</a>
+                        </div>
+                    </div>
+                </nav>
             </div>
         </nav>
     </header>
