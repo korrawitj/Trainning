93bf1336bdd6ce440d5948c08ac24fead657902e
Gunnpj <gunpengpinij@gmail.com>
2018-08-18 17:02:58 +0700

    burger menu

--- demo-lensfrontend/src/components/Navigation/Toolbar/Toolbar.js
+++ demo-lensfrontend/src/components/Navigation/Toolbar/Toolbar.js
@@ -5,7 +5,7 @@ import HomeLogo from '../../../assets/images/logo-final-2x.png';
 import './Toolbar.css';
 const toolbar = (props) => (
     <header>
-        <nav className="navbar navbar-expand-sm navbar-light fixed-top">
+        <nav className="navbar navbar-expand-sm navbar-light fixed-top main">
             <NavLink className="navbar-brand" exact to="/">
                 <img src={HomeLogo} alt="Logo" className="logo" />
             </NavLink>
@@ -25,7 +25,34 @@ const toolbar = (props) => (
                 </li>
             </ul>
         </nav>
-    </header>
+        <nav className="navbar navbar-light fixed-top burger">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+        </button>
+        <NavLink className="navbar-brand mx-auto" exact to="/">
+                <img src={HomeLogo} alt="Logo" style={{ width: "100px",backgroundColor: "black"}}  />
+            </NavLink>
+            <ul className="navbar-nav">
+                <div className="vl"></div>
+                <li className="nav-item">
+                    <i className="fa fa-cart-arrow-down nav-link" style={{ fontSize: '25px', cursor: 'pointer' }}></i>
+                </li>
+            </ul>
+        <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+                <li className="nav-item"><NavLink className="nav-link textcolor" exact to="/">หน้าหลัก</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link textcolor" to="/">รายการทั้งหมด</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link textcolor" to="/">DSLR</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link textcolor" to="/">Mirrorless</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link textcolor" to="/">GoPro</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link textcolor" to="/">วีดีโอ</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link textcolor" to="/">ขั้นตอนการเช่า/เงื่อนไข</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link textcolor" to="/About">ติดต่อเรา</NavLink></li>
+            </ul>
+  </div>
+
+        </nav>
+    </header>  
 );
 
 export default toolbar;
