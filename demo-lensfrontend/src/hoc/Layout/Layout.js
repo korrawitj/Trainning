2fecd4104cf520ab3276205a4e26683021fb9cf3
Gunnpj <gunpengpinij@gmail.com>
2018-08-26 16:39:13 +0700

    scroll top

--- demo-lensfrontend/src/hoc/Layout/Layout.js
+++ demo-lensfrontend/src/hoc/Layout/Layout.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Children from '../Children/Children';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import Footer from '../../components/Navigation/Footer/Footer';
+import ScrollToTop from '../../../src/components/ScrolltoTop/ScrolltoTop';
 // import classes from './Layout.css';
 
 class Layout extends Component {
@@ -10,9 +11,11 @@ class Layout extends Component {
         return (
             <Children>
                 <Toolbar />
-                <div className="container-fluid" style={{ marginTop: '80px' }}>
+                <div className="container-fluid" style={{ marginTop: '0rem' }}>
                     <main>
+                    <ScrollToTop>
                         {this.props.children}
+                        </ScrollToTop>
                     </main>
                 </div>
                 <Footer />
